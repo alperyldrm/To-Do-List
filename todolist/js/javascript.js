ed034a0a7ca5449ef5a74b29b8773bfa3ac6f3bd
Alper Yildirim <alperyldrm96@gmail.com>
2023-12-03 00:45:27 +0300

    firstVersion

---
+++ todolist/js/javascript.js
@@ -0,0 +1,27 @@
+let newTaskDOM = document.querySelector('#liveToastBtn');
+//newTaskDOM.addEventListener("click", newElement);
+
+let completeTaskDOM = document.querySelector("li");
+completeTaskDOM.addEventListener("click", completeTask);
+
+
+
+function newElement(){
+    var newTask = document.querySelector("#task").value; //searchteki taski alma
+    var trimmedTask = newTask.trim() // boşluklardan arındırma
+    let ulDOM = document.querySelector("ul#list");
+    let liDOM = document.createElement('li');
+    if(trimmedTask === ""){
+            //uyarı eklenecek
+    }else{
+        liDOM.innerHTML = newTask
+        ulDOM.append(liDOM)
+        document.querySelector("#task").value = "" //searchü temizleme
+    }
+}
+
+function completeTask(){
+    
+    this.style.setProperty("text-decoration", "line-through");
+    this.style.backgroundColor = "blue"
+}
