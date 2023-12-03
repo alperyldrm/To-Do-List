19fd3eb7e619caadf9158228c973b8f45586059e
Alper Yildirim <alperyldrm96@gmail.com>
2023-12-03 12:31:34 +0300

    final version with localstorage

--- todolist/js/javascript.js
+++ todolist/js/javascript.js
@@ -1,10 +1,16 @@
+const TODOLIST = localStorage.getItem("todolist");
+const TOAST = document.querySelectorAll("#liveToast");
+
+if (TODOLIST) ulDOM.innerHTML = JSON.parse(TODOLIST);
+
 function newElement(){
-    var newTask = document.querySelector("#task").value; //searchteki taski alma
+    const newTask = document.querySelector("#task").value; //searchteki taski alma
     var trimmedTask = newTask.trim() // boşluklardan arındırma
     let ulDOM = document.querySelector("ul#list");
     let liDOM = document.createElement('li');
+
     if(trimmedTask === ""){
-            //uyarı eklenecek
+        $(TOAST[1]).toast('show')
     }else{
         liDOM.innerHTML = newTask
         ulDOM.append(liDOM)
@@ -14,6 +20,8 @@ function newElement(){
         DLTSPN.classList.add("close");
         DLTSPN.addEventListener("click", () => removeElement(liDOM));
         liDOM.appendChild(DLTSPN);
+        saveToLocalStorage(ulDOM);
+        $(TOAST[0]).toast('show')
         liDOM.addEventListener("click", () => {         // Tıklanan li'ye checked classı ekleme
         if (liDOM.classList.contains("checked")) liDOM.classList.remove("checked");
         else liDOM.classList.add("checked");
@@ -28,7 +36,24 @@ item.addEventListener("click", () => {
     else item.classList.add("checked");
   });
 });
+// Listener to check class and save to local storage
+document.querySelectorAll("#list > li").forEach((item) => {
+    item.addEventListener("click", () => {
+      if (item.classList.contains("checked")) item.classList.remove("checked");
+      else item.classList.add("checked");
+      saveToLocalStorage(ulDOM);
+    });
+  });
 
 const removeElement = (element) => {
     element.remove();
   };
+
+  // Add and event listener to input field to create a new list item on enter key press
+document.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") newElement();
+  });
+
+  function saveToLocalStorage(item) {
+  localStorage.setItem("todolist", JSON.stringify(item.innerHTML));
+}
