068e87ea441862628690f796d5d28a37e638755b
Alper Yildirim <alperyldrm96@gmail.com>
2023-12-09 16:43:45 +0300

    aciklama satirlari eklendi

--- todolist/js/javascript.js
+++ todolist/js/javascript.js
@@ -6,25 +6,25 @@ if (TODOLIST) ulDOM.innerHTML = JSON.parse(TODOLIST);
 function newElement(){
     const newTask = document.querySelector("#task").value; //searchteki taski alma
     var trimmedTask = newTask.trim() // boşluklardan arındırma
-    let ulDOM = document.querySelector("ul#list");
-    let liDOM = document.createElement('li');
+    let ulDOM = document.querySelector("ul#list"); //ul elementini seçme
+    let liDOM = document.createElement('li'); // bir li elementi oluşturma
 
     if(trimmedTask === ""){
         $(TOAST[1]).toast('show')
     }else{
-        liDOM.innerHTML = newTask
-        ulDOM.append(liDOM)
+        liDOM.innerHTML = newTask //li elementinin içine newtaskı ayzma
+        ulDOM.append(liDOM) //li elementini ul elementine ekleme
         document.querySelector("#task").value = "" //searchü temizleme,
-        const DLTSPN = document.createElement("span");         // Delete butonu ekleme  
-        DLTSPN.innerHTML = "&times;";
-        DLTSPN.classList.add("close");
-        DLTSPN.addEventListener("click", () => removeElement(liDOM));
-        liDOM.appendChild(DLTSPN);
-        saveToLocalStorage(ulDOM);
-        $(TOAST[0]).toast('show')
-        liDOM.addEventListener("click", () => {         // Tıklanan li'ye checked classı ekleme
-        if (liDOM.classList.contains("checked")) liDOM.classList.remove("checked");
-        else liDOM.classList.add("checked");
+        const DLTSPN = document.createElement("span");         // Delete butonu oluşturma  
+        DLTSPN.innerHTML = "&times;"; // delete butonuna çarpı tuşu ekleme
+        DLTSPN.classList.add("close"); //bu tuşun görevinin close olduğunu class olarak ekleme
+        DLTSPN.addEventListener("click", () => removeElement(liDOM)); // çarpıya tıklanırsa removeelement fonksiyonunu çağırma
+        liDOM.appendChild(DLTSPN); // lidom'un içine oluşturulan delete butonunu ekleme
+        saveToLocalStorage(ulDOM); //uldom local storage'ına kydetme
+        $(TOAST[0]).toast('show') // toast bildirimi gösterme
+        liDOM.addEventListener("click", () => {         // li'ye tıklanırsa bu fonksiyon çalışacak
+        if (liDOM.classList.contains("checked")) liDOM.classList.remove("checked"); // checked varsa checkeddan çıkarma işlevi
+        else liDOM.classList.add("checked"); // checked yoksa checked ekleme 
   });
     }
 }
